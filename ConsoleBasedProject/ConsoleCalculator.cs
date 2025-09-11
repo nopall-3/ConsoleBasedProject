a327d0c7ce6b9ad4fe3cbb09a240d86f6df74b5a
Nopall <nopall3@gmail.com>
2025-09-11 17:12:21 +0700

    add login

---
+++ ConsoleBasedProject/ConsoleCalculator.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleBased
+{
+    internal class ConsoleCalculator
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Simple Console Calculator");
+            Console.WriteLine("-------------------------");
+
+            //input angka pertama
+            Console.Write("Masukkan Angka Pertama: ");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+
+            //input angka kedua
+            Console.Write("Masukkan Angka Kedua: ");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+
+            //pilih operasi
+            Console.WriteLine("Pilih Operator (+, -, *, /): ");
+            char operation = Console.ReadLine()[0];
+            double result = 0;
+            switch (operation)
+            {
+                case '+':
+                    result = num1 + num2;
+                    break;
+                case '-':
+                    result = num1 - num2;
+                    break;
+                case '*':
+                    result = num1 * num2;
+                    break;
+                case '/':
+                    if (num2 != 0)
+                        result = num1 / num2;
+                    else
+                        Console.WriteLine("Error: Division by zero");
+                    return;
+                default:
+                    Console.WriteLine("Error: Invalid operation");
+                    return;
+            }
+            Console.WriteLine($"Result: {num1} {operation} {num2} = {result}");
+            Console.ReadKey(); // Keep the console window open
+        }
+    }
+}
