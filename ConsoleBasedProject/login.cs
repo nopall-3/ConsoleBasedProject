c881a419236dffbdf08f7bea04275152db10cab4
Nopall <nopall3@gmail.com>
2025-09-18 16:57:32 +0700

    add login

--- ConsoleBasedProject/login.cs
+++ ConsoleBasedProject/login.cs
@@ -1,15 +1,19 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace ConsoleBasedProject
+namespace ConsoleAppIO
 {
-    internal class login
+    class Program
     {
-        public string username;
-        public string password;
-        
+        static void Main(string[] args)
+        {
+            Console.Write("Masukkan nama Anda: ");
+            string nama = Console.ReadLine();
+
+            Console.Write("Masukkan umur Anda: ");
+            int umur = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Halo " + nama + ", umur Anda " + umur + " tahun.");
+            Console.ReadKey();
+        }
     }
 }
