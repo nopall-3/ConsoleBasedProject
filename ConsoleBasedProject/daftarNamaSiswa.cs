6a087a3bdf0aa71a468a5e8a533b38e2f9d01bcf
Nopall <nopall3@gmail.com>
2025-09-25 16:54:34 +0700

    add daftarNamaSiswa

---
+++ ConsoleBasedProject/daftarNamaSiswa.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleBasedProject
+{
+    internal class daftarNamaSiswa
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("=== DAFTAR NAMA SISWA ===\n");
+            // Membuat array nama siswa
+            string[] siswa = { "Asep", "Budi", "Citra", "Dedi", "Eka" };
+
+            // Menggunakan foreach untuk menampilkan semua nama
+            foreach (string nama in siswa)
+            {
+                Console.WriteLine($"- {nama}");
+            }
+
+            Console.WriteLine("\\nProgram selesai. Tekan ENTER untuk keluar...");
+            Console.ReadLine();
+        }
+    }
+}
