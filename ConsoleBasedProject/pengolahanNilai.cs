9d673cb6ed98bab989f80f2ba343f95567a089d7
Nopall <nopall3@gmail.com>
2025-10-02 17:07:24 +0700

    add pengolahanNilai

---
+++ ConsoleBasedProject/pengolahanNilai.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleBasedProject
+{
+    internal class pengolahanNilai
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Masukkan jumlah siswa: ");
+            int jumlah = int.Parse(Console.ReadLine());
+
+            int[] nilai = new int[jumlah];
+
+            for (int i = 0; i < jumlah; i++)
+            {
+                Console.Write($"Nilai siswa ke-{i + 1}: ");
+                nilai[i] = int.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine("\n=== Hasil Kategori Nilai ===");
+            foreach (int n in nilai)
+            {
+                string kategori = "";
+                if (n >= 85) kategori = "A";
+                else if (n >= 70) kategori = "B";
+                else if (n >= 55) kategori = "C";
+                else kategori = "D";
+
+                Console.WriteLine($"Nilai {n} -> Kategori {kategori}");
+            }
+        }
+
+    }
+}
